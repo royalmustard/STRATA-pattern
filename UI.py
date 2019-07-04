6ae2cd2789dd9c09cb4e59aa6c3d14ab37938bf3
royalmustard <Felix@csruediger.de>
2019-07-04 13:42:25 +0200

    Showing image now work correctly

--- UI.py
+++ UI.py
@@ -14,19 +14,22 @@ class UI:
 		self.frame = Frame(self.root)
 		self.frame.grid(column=0, row=0, sticky=(N, E, W, S))
 
-		self.in_filename = ""
+		self.in_filename = StringVar()
+		self.in_image = None
 		self.out_filename = ""
 
 		ttk.Label(self.frame, text="Input file").grid(row=1, column=1, sticky=N)
-		ttk.Entry(self.frame, textvariable=self.in_filename).grid(row=2, column=1, sticky=N)
+		ttk.Entry(self.frame, textvariable=self.in_filename, width=50).grid(row=2, column=1, sticky=N)
 		ttk.Button(self.frame, command=self.get_path, text="Open file").grid(row=3, column=1, sticky=N)
 		self.cnv_in_image = Canvas(self.frame, width=300, height=300)
 		self.cnv_in_image.grid(row=1, column=3, sticky=N)
 
+		self.get_path()
+
 		self.frame.mainloop()
 
 	def get_path(self):
-		self.in_filename = df.askopenfilename()
-		img = Image.open(self.in_filename)
-		img = img.thumbnail([300, 300], Image.ANTIALIAS)
-		self.cnv_in_image.create_image(0, 0, anchor=NW, image=ImageTk.PhotoImage(img))
+		self.in_filename.set(df.askopenfilename())
+		self.in_image = Image.open(self.in_filename.get())
+		self.in_image = ImageTk.PhotoImage(self.in_image)
+		self.cnv_in_image.create_image((0, 0), anchor=NW, image=self.in_image)
